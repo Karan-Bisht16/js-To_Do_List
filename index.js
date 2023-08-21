dde041539feb91c46a05c7b508c2066080e15c49
Karan Bisht <134302942+Karan-Bisht16@users.noreply.github.com>
2023-08-21 21:52:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,38 @@ window.onbeforeunload = function() {
     return "Data will be lost if you leave the page, are you sure?";
 };
 
-const date_time = document.getElementById('date_time');
-const date = new Date();
 const dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const date = new Date();
+const date_time = document.getElementById('date_time');
 date_time.innerHTML = dayName[date.getDay()]+', '+monthNames[date.getMonth()]+' '+date.getDate();
 
-const mainInput = document.getElementById('mainField');
-const resultDiv = document.getElementById('result');
+const mainInput = document.querySelector('#mainField');
+const resultDiv = document.querySelector('#result');
+const searchValue = document.querySelector('#search');
+
+mainInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        addTaskOperation();
+    }
+});
+searchValue.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("searchBtn").click();
+    }
+});
 
 let i=0;
 let arrayOfTasks = [];
 
-function addTaskOperation(){
-    popUp.classList.remove('active');
+function addTaskOperation() {
     restoreOriginal();
     const taskTitle = mainInput.value.trim()
     if (taskTitle!==''){
         resultDiv.innerHTML+=
-        `<div class='visible' id='activity`+i+`'>
+        `<div class="visible" id='activity`+i+`'>
             <div class='container'>
                 <label id='label`+i+`'><input type='checkbox' onclick='strikeTask(this);' value=`+i+`> `+taskTitle+`</label>
                 <button class='removeBtn' onclick='remove(this);' value=`+i+`>Remove</button>
@@ -32,18 +45,18 @@ function addTaskOperation(){
     mainInput.value='';
 }
     
-function strikeTask(element){
-    const toStrike =document.getElementById('label'+element.value);
+function strikeTask(element) {
+    const toStrike = document.getElementById('label'+element.value);
     var string = toStrike.innerHTML;
-    if (!toStrike.classList.contains('passive')){
-        toStrike.classList.add('passive');
+    if (!toStrike.classList.contains('isChecked')){
+        toStrike.classList.add('isChecked');
         var index = toStrike.innerHTML.indexOf("value=")
         var stringFirst = string.slice(0, index);
         stringFirst+='checked ';
         string = stringFirst.concat(string.slice(index, string.length));
         toStrike.innerHTML=string;
-    } else{
-        toStrike.classList.remove('passive');
+    } else {
+        toStrike.classList.remove('isChecked');
         var index = toStrike.innerHTML.indexOf("checked")
         var stringFirst = string.slice(0, index-1);
         index+=10;
@@ -51,7 +64,7 @@ function strikeTask(element){
         toStrike.innerHTML=string;
     }
 }
-function remove(element){
+function remove(element) {
     const todelete = document.getElementById('activity'+element.value);
     todelete.innerHTML='';
     var i = parseInt(element.value);
@@ -59,11 +72,10 @@ function remove(element){
     console.log(i);
 }
 
+const popUp = document.querySelector('#popUp');
 const allTasks = document.getElementsByClassName('visible');
-const searchValue = document.getElementById('search');
-const popUp = document.getElementById('popUp');
 let flag = 0;
-function searchTask(){
+function searchTask() {
     popUp.classList.remove('active');
     for (var i=0; i<arrayOfTasks.length; i++){
         allTasks[i].classList.add('hidden');  
@@ -81,21 +93,10 @@ function searchTask(){
     searchValue.value='';
     flag=0;
 }
+
 function restoreOriginal(){
-    for (var i=0; i<arrayOfTasks.length; i++){
-        allTasks[i].classList.remove('hidden');                    
+    for (let j=0; j<arrayOfTasks.length; j++){
+        allTasks[j].classList.remove('hidden');                    
     }
     popUp.classList.remove('active');
 }
-mainInput.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        addTaskOperation();
-    }
-});
-searchValue.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("searchBtn").click();
-    }
-});
